65ef7ece6c5dff49bb422f89b6a881fda3fda1be
Natalia Blanco <UO295340@uniovi.es>
2025-10-01 21:00:50 +0200

    Primer commit desde proyecto local (base proyecto)

---
+++ js/juegos.js
@@ -0,0 +1,9 @@
+// Redirigimos al hacer clic en "La cuenta" a la página de la cuenta
+document.getElementById("btn-cuenta").addEventListener("click", function() {
+    window.location.href = "laCuenta.html"
+})
+
+// Redirigimos al hacer clic en "Scrable" a la página de scrable
+document.getElementById("btn-scrable").addEventListener("click", function() {
+    window.location.href = "scrable.html"
+})
